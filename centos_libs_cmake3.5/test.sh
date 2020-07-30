da90d99cca1aee612c636f00519bed37490d94c4
Karol Sierociński <ksiero@man.poznan.pl>
2020-07-30 11:24:36 +0200

    Docker images tests (#15)
    * Docker images tests
    
    * Disable ctest for cmake3.10
    
    * Test script for each image
    
    * centos images test sudo from /bin
    
    * Inner bash string for user call
    
    * Typo fix
    
    * Curl installed by default
    
    * Fix typos
    
    * Make multithread

---
+++ centos_libs_cmake3.5/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -t pdidevel/centos_libs_cmake3.5 /bin/bash -c \
+                                            "curl -Lso pdi.tar.gz https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/master/pdi-master.tar.gz && \
+                                            tar -xf pdi.tar.gz && \
+                                            cd pdi* && \
+                                            cmake -DDIST_PROFILE=Devel -DUSE_DEFAULT=SYSTEM -DBUILD_DOCUMENTATION=OFF -DBUILD_TESTING=OFF . && \
+                                            make -j"
---
+++ xenial_libs_cmake3.10/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run -t pdidevel/xenial_libs_cmake3.10 /bin/bash -c \
+                                            "curl -Lso pdi.tar.gz https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/master/pdi-master.tar.gz && \
+                                            tar -xf pdi.tar.gz && \
+                                            cd pdi* && \
+                                            cmake -DDIST_PROFILE=Devel -DUSE_DEFAULT=SYSTEM -DUSE_Bpp=SYSTEM . && \
+                                            make -j && \
+                                            ctest --output-on-failure"
