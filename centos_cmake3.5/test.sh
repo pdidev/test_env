8a6af26fe3cedb2d720a4ee43de69822c8a5e1ad
Karol Sierociński <ksiero@man.poznan.pl>
2020-09-16 17:24:58 +0200

    Using external build script from PDI master (#20)

--- centos_cmake3.5/test.sh
+++ centos_cmake3.5/test.sh
@@ -4,5 +4,4 @@ docker run -t pdidevel/centos_cmake3.5 /bin/bash -c \
                                         "curl -Lso pdi.tar.gz https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/master/pdi-master.tar.gz && \
                                         tar -xf pdi.tar.gz && \
                                         cd pdi* && \
-                                        cmake -DDIST_PROFILE=Devel -DUSE_DEFAULT=EMBEDDED -DBUILD_DOCUMENTATION=OFF -DBUILD_TESTING=OFF . && \
-                                        make -j"
+                                        sh tools/build_scripts/centos_cmake3.5_embedded.sh"
--- centos_libs_cmake3.10/test.sh
+++ centos_libs_cmake3.10/test.sh
@@ -4,6 +4,4 @@ docker run -t pdidevel/centos_libs_cmake3.10 /bin/bash -c \
                                             "curl -Lso pdi.tar.gz https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/master/pdi-master.tar.gz && \
                                             tar -xf pdi.tar.gz && \
                                             cd pdi* && \
-                                            cmake -DDIST_PROFILE=Devel -DUSE_DEFAULT=SYSTEM . && \
-                                            make -j && \
-                                            ctest --output-on-failure"
+                                            sh tools/build_scripts/centos_cmake3.10_system.sh"
