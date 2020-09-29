afa39794e75262fc234ebd2997109ca4035db183
Julien Bigot <jbigot@users.noreply.github.com>
2020-09-29 12:45:13 +0200

    use a git clone instead of a tar (#23)
    * Enable ctest for centos
    
    * Disable ctest on CentOS github CI
    
    * Moved bash script to docker_images.yml
    
    * Docker echo test
    
    * Get PDI and untar
    
    * use a git clone instead of a tar
    
    * direct use of git instead of github only "action"
    
    * export env about current test version
    
    Co-authored-by: Karol Sierocinski <ksiero@man.poznan.pl>

--- xenial_libs_cmake3.10/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -t pdidevel/xenial_libs_cmake3.10 /bin/bash -c \
-                                            "curl -Lso pdi.tar.gz https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/master/pdi-master.tar.gz && \
-                                            tar -xf pdi.tar.gz && \
-                                            cd pdi* && \
-                                            sh tools/build_scripts/xenial_cmake3.10_system.sh"
