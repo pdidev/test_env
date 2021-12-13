987608541fb89d098a1aab5f18aa88093ef07a60
Julien Bigot <julien.bigot@cea.fr>
2021-12-13 15:51:44 +0100

    Reworked

--- .github/detect_version.sh
+++ .github/detect_version.sh
@@ -8,9 +8,9 @@ echo "BRANCH_NAME=${BRANCH_NAME}"
 set +e
 if echo "${BRANCH_NAME}" | grep -qs '^v[0-9]*$' &> /dev/null
 then
-	image_version="$(echo "${BRANCH_NAME}" | sed 's%^v\([0-9]*\)$%\1%')"
+	image_version="v$(echo "${BRANCH_NAME}" | sed 's%^v\([0-9]*\)$%\1%')"
 else
-	image_version="$(( 0$(git branch -r --merged | grep '^\s*origin/v[0-9]*\s*$' | sed 's%^\s*origin/v\([0-9]*\)\s*$%\1%' | sort -nu | head -n 1 ) +1 ))_${GITHUB_SHA}"
+	image_version="v$(( 0$(git branch -r --merged | grep '^\s*origin/v[0-9]*\s*$' | sed 's%^\s*origin/v\([0-9]*\)\s*$%\1%' | sort -nu | head -n 1 ) +1 ))_pre_${GITHUB_SHA:0:7}"
 fi
-echo "image_version=v${image_version}"
-echo "image_version=v${image_version}" >> $GITHUB_ENV
+echo "image_version=${image_version}"
+echo "image_version=${image_version}" >> $GITHUB_ENV
