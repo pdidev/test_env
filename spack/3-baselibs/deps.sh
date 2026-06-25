b5ddbb4e2671614faa478904dceb51bfab640b00
jacques.morice <jacques.morice@cea.Fr>
2026-06-25 15:04:46 +0200

    debuntu with catalyst

--- spack/3-baselibs/deps.sh
+++ spack/3-baselibs/deps.sh
@@ -3,7 +3,7 @@ BASE_BUILDDEPS="cmake doxygen pkgconf py-setuptools"
 # libraries that are required in all cases
 BASE_LIBS="py-numpy py-pyyaml python"
 # libraries that depend on MPI & are required in all cases
-BASE_MPILIBS="py-mpi4py"
+BASE_MPILIBS="py-mpi4py catalyst@2.0.0+mpi+python"
 # libraries that are optional because embedded in PDI
 OPT_LIBS="paraconf py-pybind11 spdlog"
 # libraries that depend on MPI & are optional because embedded in PDI
